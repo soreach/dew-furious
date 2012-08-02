bbcd54738c1143b1b8511a1b6c2018f61b6be28b
pp7282000 <pp7282000@gmail.com>
2012-08-02 01:39:43 +0000

    ClienteController change

--- HelloWorldSpring3/src/net/furious/controllers/ClienteController.java
+++ HelloWorldSpring3/src/net/furious/controllers/ClienteController.java
@@ -5,6 +5,8 @@ import net.vetfurious.excepcion.DAOExcepcion;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -28,9 +30,8 @@ public class ClienteController {
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException, DAOExcepcion {
 		
-		
-		List<Cliente> clientes = this.Cliente.listClientes();
-		
+		List<Cliente> clientes = this.Cliente.listarprospectos();
+				
 		Map<String, Object> myModel = new HashMap<String, Object>();
 		myModel.put("clientes",clientes);
        
@@ -60,7 +61,7 @@ public class ClienteController {
 		oModelCliente.setEmail(request.getParameter("txtEmail"));
 		oModelCliente.setImagen(request.getParameter("txtImagen"));
 		oModelCliente.setTelefono(request.getParameter("txtTelefono"));
-
+		
 		try {
 			Cliente.GrabarModificarProspecto(oModelCliente);
 		} catch (Exception e) {
@@ -130,5 +131,95 @@ public class ClienteController {
 		return new ModelAndView("redirect:listarcliente.jsp");
 	}
 
+	
+	@RequestMapping(value = "/Menu", method = RequestMethod.GET)
+	public ModelAndView IrMenu(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("Menu");
+		//String RutaFile=request.getContextPath();
+		//return new ModelAndView(RutaFile + "/index");
+		return new ModelAndView("index");
+	}
+	
+	@RequestMapping(value = "/CambiarCliente", method = RequestMethod.GET)
+	public ModelAndView CambiarCliente(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("CambiarCliente");
+		
+		Cliente oModelCliente = new Cliente();
+		oModelCliente.setCodigo(request.getParameter("id"));
+		
+		try {
+			Cliente.changeProspectoToCliente(oModelCliente.getCodigo());
+		} catch (Exception e) {
+			return new ModelAndView("/error", "mensaje", e.getMessage());
+		}
 
+		return new ModelAndView("redirect:listarcliente.jsp");
+	}
+
+	@RequestMapping(value="/listarcliente2.jsp")
+	public ModelAndView ListarClientes(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, DAOExcepcion {
+		
+		List<Cliente> clientes = this.Cliente.listClientes();
+		Map<String, Object> myModel = new HashMap<String, Object>();
+		myModel.put("clientes",clientes);
+		
+        return new ModelAndView("listarclienteC", "model", myModel);
+        
+    }
+	
+	@RequestMapping(value = "/eliminarClienteC", method = RequestMethod.GET)
+	public ModelAndView eliminarClienteC(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("eliminarClienteC");
+		
+		Cliente oModelCliente = new Cliente();
+		oModelCliente.setCodigo(request.getParameter("id"));
+	
+		try {
+			Cliente.deleteprospecto(oModelCliente.getCodigo());
+		} catch (Exception e) {
+			return new ModelAndView("/error", "mensaje", e.getMessage());
+		}
+
+		return new ModelAndView("redirect:listarcliente2.jsp");
+	}
+	
+	
+	@RequestMapping(value = "/editarClienteC", method = RequestMethod.GET)
+	public ModelAndView editarClienteC(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		Cliente oModelCliente = new Cliente();
+		oModelCliente = Cliente.ObtenerProspecto(request.getParameter("id"));
+		
+		System.out.println("idCliente "+request.getParameter("id"));
+		return new ModelAndView("editarClienteC", "model", oModelCliente);
+	}
+	
+	@RequestMapping(value = "/guardarModificacionClienteC", method = RequestMethod.POST)
+	public ModelAndView modificarClienteC(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		System.out.println("guardarModificacionClienteC");
+		
+		Cliente oModelCliente = new Cliente();
+		oModelCliente.setCodigo(request.getParameter("id"));
+		oModelCliente.setNombre(request.getParameter("txtNombre"));
+		oModelCliente.setApellidopaterno(request.getParameter("txtApellidoPaterno"));
+		oModelCliente.setApellidomaterno(request.getParameter("txtApellidoMaterno"));
+		oModelCliente.setCelular(request.getParameter("txtCelular"));
+		oModelCliente.setDireccion(request.getParameter("txtDireccion"));
+		oModelCliente.setEmail(request.getParameter("txtEmail"));
+		oModelCliente.setImagen(request.getParameter("txtImagen"));
+		oModelCliente.setTelefono(request.getParameter("txtTelefono"));
+		
+		try {
+			Cliente.GrabarModificarProspecto(oModelCliente);
+		} catch (Exception e) {
+			return new ModelAndView("/error", "mensaje", e.getMessage());
+		}
+
+		return new ModelAndView("redirect:listarcliente2.jsp");
+	}
 }
